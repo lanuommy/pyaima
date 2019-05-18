6cebf27a15b7982d678687f0e7dfdbbcdc591690
lanuommy <39019000+lanuommy@users.noreply.github.com>
2019-05-18 10:38:28 +0800

    Update aima.py

--- aima.py
+++ aima.py
@@ -1,4 +1,3 @@
-# -*- coding: UTF-8 -*-
 import json
 import _json
 import sys
@@ -10,7 +9,7 @@ from bs4 import BeautifulSoup
 
 #创建excel
 book = xlwt.Workbook(encoding='utf-8',style_compression=0)
-
+#根据爱玛电动车在前端写死的select里，找出规则，按顺序排列
 cityList = [
 
 "北京",
@@ -45,22 +44,32 @@ cityList = [
  "浙江",
  "重庆"
 ]
-
+#定义book，创建sheet
+sheet = book.add_sheet('list', cell_overwrite_ok=True)
+count = 0
 for i in range(2,33):
     info = requests.get("https://www.aimatech.com/outlets/list?ccode=0&pcode=%s"%(i))
     jsonall = json.loads(info.text[11:-1])
-    sheet = book.add_sheet('list', cell_overwrite_ok=True)
+
     print("已经写完第一条数据，进入第二条")
     # 写入表头，定义好列
-    sheet.write(0, 0, label='门店地址')
-    sheet.write(0, 1, label='门店名字')
-    sheet.write(0, 2, label='联系电话')
+    sheet.write(0, 0, label='城市名字')
+    sheet.write(0, 1, label='门店地址')
+    sheet.write(0, 2, label='门店名字')
+    sheet.write(0, 3, label='联系电话')
     #    print(jsonall)
     for a in jsonall:
-        print(i)
         # 添加sheet
         addr = a['addr']
         name = a['realname']
         phonenumber = a['mobile']
 
-book.save('/Users/qiao/爱玛电动车所有门店信息.xls')
+
+        count = count + 1
+        
+        sheet.write(count, 0, cityList[i-2])
+        sheet.write(count, 1, addr)
+        sheet.write(count, 2, name)
+        sheet.write(count, 3, phonenumber)
+
+book.save('D:\Python_files\爱玛电动车所有门店信息.xls')
